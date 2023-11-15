7615bda1e6cfbf2a93245cbec950151393d4d984
unknown <ferfagner@gmail.com>
2023-11-15 15:23:08 -0300

    criando configurando pagina de callback

--- src/pages/Callback/callback.tsx
+++ src/pages/Callback/callback.tsx
@@ -1,31 +1,41 @@
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Callback(){
+    const { code } = useParams();
 
     async function fetchData () {
-        try {
-          const response = await axios.get('https://www.bling.com.br/Api/v3/oauth/authorize?response_type=code&client_id=e974c2fc68a604ad298603299b776b82702f2e0a&state=ceb128688bdb1d2c229a0bc14192c95b&scopes=98308+98313+98619+101584+104163+507943+1563512+200802821+318257559+318257561+318257565', {
-           
-          });
-  
-          // Você não pode acessar diretamente o corpo da resposta em 'no-cors'
-          // Pode verificar se a solicitação foi bem-sucedida sem o corpo
-          if (response) {
-            console.log('Solicitação bem-sucedida');
-          } else {
-            console.log('Erro na solicitação');
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      };
         
+        const autorization = atob(`${process.env.BLING_CLIENT_ID}:${process.env.BLING_CLIENT_SICRET}`)
+
+        const data = new URLSearchParams();
+        data.append('grant_type', 'authorization_code');
+        data.append('code', `${code}`);
+        
+        const headers = {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Accept': '1.0',
+          'Authorization': `Basic ${autorization}`,
+        };
+        
+       await axios.post('https://www.bling.com.br/Api/v3/oauth/token', data, { headers })
+          .then(response => {
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+        }
+
+        useEffect(()=>{
+            fetchData()
+        },[])
     
     return(
         <>
-        <button onClick={()=>{fetchData()}}>
-            Teste
-        </button>
+       <h2>{code}</h2>
         </>
     )
+
 }
