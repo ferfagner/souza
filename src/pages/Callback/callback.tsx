107a0744b9d5892193f43900861a04a2b5472dd9
unknown <ferfagner@gmail.com>
2023-11-15 20:16:15 -0300

    atualizando autenticacao bling

--- src/pages/Callback/callback.tsx
+++ src/pages/Callback/callback.tsx
@@ -4,27 +4,29 @@ import {  useSearchParams} from 'react-router-dom';
 
 export default function Callback(){
     let [searchParams] = useSearchParams();
+    const redirectUri = 'https://souza-beige.vercel.app/callback';
 
     let code = searchParams.get("code")
-
+    const tokenUrl = 'https://www.bling.com.br/Api/v3/oauth/token';
 
     async function fetchData () {
         const autorization = btoa(`${process.env.BLING_CLIENT_ID}:${process.env.BLING_CLIENT_SICRET}`)
       
-        const data = {
-            'grant_type': 'authorization_code',
-            'code': `${code}`,
-          };
-
-        const headers = {
-         'Content-Type': 'application/x-www-form-urlencoded',
-         'Accept': '1.0',
-         'Authorization': `Basic ${autorization}`,
-            };
-        axios.post('https://www.bling.com.br/Api/v3/oauth/token', data ,{ headers})
-          .then(response => {
-            console.log(response.data);
-          })
+            axios.post(tokenUrl, {
+                grant_type: 'authorization_code',
+                code: code,
+                client_id: process.env.BLING_CLIENT_ID,
+                client_secret: process.env.BLING_CLIENT_SICRET,
+                redirect_uri: redirectUri
+            }, {
+                headers: {
+                    'Authorization': `Basic ${autorization}` ,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Accept': '1.0'
+                }
+            }).then((response)=>{
+                console.log('Token de acesso:', response.data.access_token);
+            })
           .catch(error => {
             console.error(error);
           });
