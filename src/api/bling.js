5824e2ecb6a949c7b3700338ef7e9670fec4e6ee
unknown <ferfagner@gmail.com>
2023-11-15 21:10:21 -0300

    adicionando api

--- src/api/bling.js
+++ src/api/bling.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
-import { useEffect } from 'react';
 import {  useSearchParams} from 'react-router-dom';
 
-export default function Callback(){
+export default async function handler(req, res){
     let [searchParams] = useSearchParams();
 
     let code = searchParams.get("code")
     const tokenUrl = 'https://www.bling.com.br/Api/v3/oauth/token';
 
-    async function fetchData () {
         const autorization = btoa(`${process.env.BLING_CLIENT_ID}:${process.env.BLING_CLIENT_SICRET}`)
       
             axios.post(tokenUrl, {
@@ -26,16 +24,8 @@ export default function Callback(){
           .catch(error => {
             console.log(error);
           });
-        }
+        
 
-        useEffect(()=>{
-           fetchData()
-        },[])
     
-    return(
-        <>
-       <h2>a</h2>
-        </>
-    )
 
 }
